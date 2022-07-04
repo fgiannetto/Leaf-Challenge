da12b708463e5649c413bbde0ca6799b2eb9e302
fgiannetto <107591457+fgiannetto@users.noreply.github.com>
2022-07-04 14:55:38 -0300

    Update_ch-2.py
    Corrección del challenge #2.
    Se agrega función para introducir la matriz deseada manualmente.

--- challenge-2/ch-2.py
+++ challenge-2/ch-2.py
@@ -1,11 +1,22 @@
 #Función que recibe una lista de listas y la devuelve identificando si el numero es par o impar
 
 
-matriz = [[1,2,3], [2,3,4],[4,5,6]]
+filas = int(input ("Introduzca número de filas: "))
+columnas = int(input ("Introduzca número de columnas: "))
+print("\n")
 
+matriz = []
+#matriz = [[1,2,3], [2,3,4],[4,5,6]]
 lista = []
+lista_nueva = []
 
-for m in range(3):
+for i in range (filas):
+    matriz.append([])
+    for j in range(columnas):
+        numero = int(input("Fila #{}, Columna#{} -> ".format(i+1, j+1)))
+        matriz[i].append(numero)
+
+for m in range(len(matriz)):
     for num in matriz[m]:
         if (num % 2) == 0:  
             #print("True".format(num))
@@ -13,5 +24,7 @@ for m in range(3):
         else:  
             #print("False".format(num))
             lista.append('False')
-        
-print (lista)
+    lista_nueva.append(lista)
+    lista = []
+
+print (lista_nueva)
